7b4d9ec5a52c65d2fe585fbf70be8738f06db2cf
Trivium-bv <trivium.bv@gmail.com>
2025-02-06 23:10:14 +0300

    evenOrOdd

---
+++ 8kyu/even-or-odd.js
@@ -0,0 +1,7 @@
+// DESCRIPTION:
+// If the number is odd, then dividing by two would have a non-zero remainder.
+
+// SOLUTION:
+function evenOrOdd(number) {
+    return number % 2 === 0 ? 'Even' : 'Odd';
+     }
