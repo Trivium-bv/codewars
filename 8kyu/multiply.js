2bb4f4a6c866246ae38127a07259c91294217899
Trivium-bv <trivium.bv@gmail.com>
2025-02-06 23:39:15 +0300

    multiply

---
+++ 8kyu/multiply.js
@@ -0,0 +1,9 @@
+// ❔DESCRIPTION:
+// This code does not execute properly. Try to figure out why.
+
+// ✅ SOLUTION:
+function multiply(a, b){
+    return a * b
+  }
+console.log("multiply: ", multiply(3, 5))
+console.log("multiply: ", multiply(1, 5))
